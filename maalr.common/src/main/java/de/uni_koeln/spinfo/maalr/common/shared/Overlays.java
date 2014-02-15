13fb6187218c78304c8401601704353f75c42f3b
Stephan Schwiebert <stephan.schwiebert@gmail.com>
2014-02-15 19:56:26 +1100

    made overlays.xml file optional

--- maalr.common/src/main/java/de/uni_koeln/spinfo/maalr/common/shared/Overlays.java
+++ maalr.common/src/main/java/de/uni_koeln/spinfo/maalr/common/shared/Overlays.java
@@ -16,6 +16,7 @@
 package de.uni_koeln.spinfo.maalr.common.shared;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -56,24 +57,29 @@ public class Overlays {
 		try {
 			JAXBContext ctx = JAXBContext.newInstance(Overlays.class, OverlayEditor.class);
 			Unmarshaller unmarshaller = ctx.createUnmarshaller();
-			instance = (Overlays) unmarshaller.unmarshal(new InputStreamReader(new FileInputStream("maalr_config/overlays.xml"), "UTF-8"));
-			List<Overlay> overlays = instance.xmlList;
-			for (Overlay overlay : overlays) {
-				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(overlay.getForm()), "UTF-8"));
-				String line = null;
-				StringBuilder sb = new StringBuilder();
-				while((line = br.readLine()) != null) {
-					sb.append(line);
-					sb.append("\n");
+			File overlayDefinition = new File("maalr_config/overlays.xml");
+			if(overlayDefinition.exists()) {
+				instance = (Overlays) unmarshaller.unmarshal(new InputStreamReader(new FileInputStream(overlayDefinition), "UTF-8"));
+				List<Overlay> overlays = instance.xmlList;
+				for (Overlay overlay : overlays) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(overlay.getForm()), "UTF-8"));
+					String line = null;
+					StringBuilder sb = new StringBuilder();
+					while((line = br.readLine()) != null) {
+						sb.append(line);
+						sb.append("\n");
+					}
+					br.close();
+					overlay.setForm(sb.toString());
+					String editorPath = overlay.getEditor();
+					if(editorPath != null) {
+						OverlayEditor editor = (OverlayEditor) unmarshaller.unmarshal(new InputStreamReader(new FileInputStream(editorPath), "UTF-8"));
+						instance.editors.put(overlay.getType(), editor);
+					}
+					instance.overlays.put(overlay.getType(), overlay);
 				}
-				br.close();
-				overlay.setForm(sb.toString());
-				String editorPath = overlay.getEditor();
-				if(editorPath != null) {
-					OverlayEditor editor = (OverlayEditor) unmarshaller.unmarshal(new InputStreamReader(new FileInputStream(editorPath), "UTF-8"));
-					instance.editors.put(overlay.getType(), editor);
-				}
-				instance.overlays.put(overlay.getType(), overlay);
+			} else {
+				instance = new Overlays();
 			}
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
@@ -113,11 +119,13 @@ public class Overlays {
 	}
 
 	public static OverlayEditor getEditor(String overlayType) {
+		if(instance.editors == null) return null;
 		return instance.editors.get(overlayType);
 	}
 
 	public static ArrayList<String> getOverlayTypes(boolean firstLanguage) {
 		ArrayList<String> types = new ArrayList<String>();
+		if(instance.overlays == null) return types;
 		Collection<Overlay> overlays = instance.overlays.values();
 		for (Overlay overlay : overlays) {
 			if(overlay.isFirstLanguage() == firstLanguage) {
