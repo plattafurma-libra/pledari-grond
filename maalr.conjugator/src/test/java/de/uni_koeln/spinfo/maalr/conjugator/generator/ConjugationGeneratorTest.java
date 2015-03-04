7c5b5161eac71b54f91705eceb53db742879353b
fmondaca <f.mondaca@uni-koeln.de>
2015-03-04 12:18:42 +0100

    maalr surmiran - conjugator up

--- maalr.conjugator/src/test/java/de/uni_koeln/spinfo/maalr/conjugator/generator/ConjugationGeneratorTest.java
+++ maalr.conjugator/src/test/java/de/uni_koeln/spinfo/maalr/conjugator/generator/ConjugationGeneratorTest.java
@@ -10,13 +10,12 @@ import org.junit.Test;
 public class ConjugationGeneratorTest {
 
 	private static ConjugationGenerator generator;
-	private static String query;
 	private HashMap<String, String> conjugation;
 
 	@BeforeClass
 	public static void initialize() {
 		generator = new ConjugationGenerator();
-		//query = "s'infurmar";
+
 
 	}
 
@@ -24,19 +23,12 @@ public class ConjugationGeneratorTest {
 	public void testGenerateConjugation() throws FileNotFoundException,
 			IOException {
 
-		conjugation = generator.generateConjugation("bugnier", 9);
+		conjugation = generator.generateConjugation("sa tar", 1);
 
-		generator.printConjugation(conjugation, "ART-9_bugnier");
-
-	}
-
-	@Test
-	public void changeVocalInRoot() {
-
-		String s = generator.changeVocalInRoot("confirm");
-
-		System.out.println(s);
+		generator.printConjugation(conjugation, "ART-9_s'eir");
 
 	}
+
+
 
 }
