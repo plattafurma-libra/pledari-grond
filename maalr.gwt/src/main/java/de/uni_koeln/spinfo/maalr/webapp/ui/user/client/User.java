c48941271d92fe2c1360fd4f7d11de07a233995a
Mihail Atanassov <atanassov.mihail@gmail.com>
2015-05-04 16:10:37 +0200

    Show all external links in index.jsp

--- maalr.gwt/src/main/java/de/uni_koeln/spinfo/maalr/webapp/ui/user/client/User.java
+++ maalr.gwt/src/main/java/de/uni_koeln/spinfo/maalr/webapp/ui/user/client/User.java
@@ -15,6 +15,8 @@
  ******************************************************************************/
 package de.uni_koeln.spinfo.maalr.webapp.ui.user.client;
 
+import java.util.List;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
@@ -24,6 +26,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.i18n.client.Dictionary;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.History;
@@ -67,36 +70,36 @@ public class User implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		this.service = GWT.create(SearchService.class);
-		search = new Search();
-		resultCellTable = new ResultCellTable();
-		AsyncLemmaDescriptionLoader.afterLemmaDescriptionLoaded(new AsyncCallback<LemmaDescription>() {
-
-					@Override
-					public void onFailure(Throwable caught) {
-					}
-
-					@Override
-					public void onSuccess(LemmaDescription result) {
-						HiJax.hijackAnchor("propose_navi", new Command() {
-
-							@Override
-							public void execute() {
-								openEditor();
-							}
-						});
-						try {
+//		this.service = GWT.create(SearchService.class);
+//		search = new Search();
+//		resultCellTable = new ResultCellTable();
+//		AsyncLemmaDescriptionLoader.afterLemmaDescriptionLoaded(new AsyncCallback<LemmaDescription>() {
+//
+//					@Override
+//					public void onFailure(Throwable caught) {
+//					}
+//
+//					@Override
+//					public void onSuccess(LemmaDescription result) {
+//						HiJax.hijackAnchor("propose_navi", new Command() {
+//
+//							@Override
+//							public void execute() {
+//								openEditor();
+//							}
+//						});
+//						try {
 							initializeMainPanel();
-						} catch (Exception e) {
-							logger.error("Error!", e);
-							Window.alert("Please check log file: " + e);
-						}
-					}
-				});
-		Element element = DOM.getElementById("languages-widget");
-		if(element != null) {
-			updateLanguageLinks(element);
-		}
+//						} catch (Exception e) {
+//							logger.error("Error!", e);
+//							Window.alert("Please check log file: " + e);
+//						}
+//					}
+//				});
+//		Element element = DOM.getElementById("languages-widget");
+//		if(element != null) {
+//			updateLanguageLinks(element);
+//		}
 	}
 
 	private void updateLanguageLinks(Element element) {
@@ -128,83 +131,101 @@ public class User implements EntryPoint {
 
 	private void initializeMainPanel() {
 		
-		Element noScriptDiv = DOM.getElementById("nojs_searchcontainer");
-		if(noScriptDiv != null)
-			noScriptDiv.removeFromParent();
-	
-		search.setResultCellTable(resultCellTable);
-		search.addSearchHandler(new SearchHandler() {
-
-			@Override
-			public void onSearch(final SearchEvent event) {
-
-				AsyncLemmaDescriptionLoader
-						.afterLemmaDescriptionLoaded(new AsyncCallback<LemmaDescription>() {
-
-							@Override
-							public void onFailure(Throwable caught) {
-								// TODO Auto-generated method stub
-							}
-
-							@Override
-							public void onSuccess(LemmaDescription result) {
-								// TODO Auto-generated method stub
-							}
-						});
-			}
-
-		});
-		search.addPagerHandler(new PagerHandler() {
-
-			@Override
-			public void onSizeChanged(PagerEvent event) {
-				resultCellTable.doPageSizeChanged(event.getSize());
-			}
-
-		});
+//		Element noScriptDiv = DOM.getElementById("nojs_searchcontainer");
+//		if(noScriptDiv != null)
+//			noScriptDiv.removeFromParent();
+//	
+//		search.setResultCellTable(resultCellTable);
+//		search.addSearchHandler(new SearchHandler() {
+//
+//			@Override
+//			public void onSearch(final SearchEvent event) {
+//
+//				AsyncLemmaDescriptionLoader
+//						.afterLemmaDescriptionLoaded(new AsyncCallback<LemmaDescription>() {
+//
+//							@Override
+//							public void onFailure(Throwable caught) {
+//								// TODO Auto-generated method stub
+//							}
+//
+//							@Override
+//							public void onSuccess(LemmaDescription result) {
+//								// TODO Auto-generated method stub
+//							}
+//						});
+//			}
+//
+//		});
+//		search.addPagerHandler(new PagerHandler() {
+//
+//			@Override
+//			public void onSizeChanged(PagerEvent event) {
+//				resultCellTable.doPageSizeChanged(event.getSize());
+//			}
+//
+//		});
+//		
+//		// Insert search widget into div#content 
+//		RootPanel contentPanel = RootPanel.get("content");
+//		if(contentPanel != null) {
+//			DictLinksDropDown dictLinksDropDown = new DictLinksDropDown();
+//			RootPanel.get("navi_head").add(dictLinksDropDown);
+//			contentPanel.add(search);
+//		}
+//
+//
+//		History.addValueChangeHandler(new ValueChangeHandler<String>() {
+//			public void onValueChange(ValueChangeEvent<String> event) {
+//				String historyToken = event.getValue();
+//				MaalrQuery maalrQuery = MaalrQuery.parse(historyToken);
+//				search.updateUI(maalrQuery);
+//				doSearch(maalrQuery);
+//			}
+//		});
+//
+//		if (History.getToken() != null) {
+//			//Logger.getLogger(getClass()).info("History.getToken(): " + History.getToken());
+//			History.fireCurrentHistoryState();
+//		}
 		
-		// Insert search widget into div#content 
-		RootPanel contentPanel = RootPanel.get("content");
-		if(contentPanel != null) {
-			DictLinksDropDown dictLinksDropDown = new DictLinksDropDown();
-			RootPanel.get("navi_head").add(dictLinksDropDown);
-			contentPanel.add(search);
-		}
-
-
-		History.addValueChangeHandler(new ValueChangeHandler<String>() {
-			public void onValueChange(ValueChangeEvent<String> event) {
-				String historyToken = event.getValue();
-				MaalrQuery maalrQuery = MaalrQuery.parse(historyToken);
-				search.updateUI(maalrQuery);
-				doSearch(maalrQuery);
-			}
-		});
-
-		if (History.getToken() != null) {
-			//Logger.getLogger(getClass()).info("History.getToken(): " + History.getToken());
-			History.fireCurrentHistoryState();
-		}
+		hiJackExternalLinks("links_ulteriurs", DictionaryConstants.DICT_LINKS_EXTERNAL, DictionaryConstants.getExtLinksDictionary());
+		hiJackExternalLinks("link_glossaris", DictionaryConstants.GLOSSAR_LINKS, DictionaryConstants.getLinksDictionary());
 		
-		Element anchorToOtherDicts = DOM.getElementById("links_ulteriurs");
-		if(anchorToOtherDicts != null) {
-			Anchor wrapper = Anchor.wrap(anchorToOtherDicts);
+//		Element anchorToOtherDicts = DOM.getElementById("links_ulteriurs");
+//		if(anchorToOtherDicts != null) {
+//			Anchor wrapper = Anchor.wrap(anchorToOtherDicts);
+//			wrapper.addClickHandler(new ClickHandler() {
+//
+//				@Override
+//				public void onClick(ClickEvent event) {
+//					new ExternalLinkDialog(DictionaryConstants.DICT_LINKS_EXTERNAL,
+//							DictionaryConstants.getExtLinksDictionary());
+//					event.getNativeEvent().preventDefault();
+//					event.getNativeEvent().stopPropagation();
+//				}
+//			});
+//		}		
+//		search.setFocus(true);
+	}
+
+
+	private void hiJackExternalLinks(final String linkId, final List<String> dictLinksList, final Dictionary dictionary) {
+		Element anchor = DOM.getElementById(linkId);
+		if(anchor != null) {
+			Anchor wrapper = Anchor.wrap(anchor);
 			wrapper.addClickHandler(new ClickHandler() {
-
 				@Override
 				public void onClick(ClickEvent event) {
-					new ExternalLinkDialog(DictionaryConstants.DICT_LINKS_EXTERNAL,
-							DictionaryConstants.getExtLinksDictionary());
+					new ExternalLinkDialog(dictLinksList, dictionary);
 					event.getNativeEvent().preventDefault();
 					event.getNativeEvent().stopPropagation();
 				}
 			});
 		}
 		
-		
-		search.setFocus(true);
 	}
-
+	
 	private void doSearch(final MaalrQuery maalrQuery) {
 		if(maalrQuery.getValues().size() == 0) {
 			return;
