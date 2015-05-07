1c2b11f4b050945d8c19977e574a70ae46ba30bf
fmondaca <f.mondaca@uni-koeln.de>
2015-05-07 14:37:21 +0200

    Reflex added

---
+++ maalr.conjugator/src/main/java/de/uni_koeln/spinfo/maalr/conjugator/verbs/Reflex.java
@@ -0,0 +1,25 @@
+package de.uni_koeln.spinfo.maalr.conjugator.verbs;
+
+public class Reflex {
+
+	
+	private String verb;
+	private String prefix;
+	
+	
+	public String getVerb() {
+		return verb;
+	}
+	public void setVerb(String verb) {
+		this.verb = verb;
+	}
+	public String getPrefix() {
+		return prefix;
+	}
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	
+	
+	
+}
