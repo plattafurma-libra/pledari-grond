204e2aca42d57ed873edcaea728d62f87180ccbc
Mihail Atanassov <atanassov.mihail@gmail.com>
2015-06-05 13:06:49 +0200

    Add i18n to user module via Constants

---
+++ maalr.gwt/src/main/java/de/uni_koeln/spinfo/maalr/webapp/ui/user/client/UserConstants.java
@@ -0,0 +1,11 @@
+package de.uni_koeln.spinfo.maalr.webapp.ui.user.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface UserConstants extends Constants {
+	
+	public String dictionaries();
+	
+	public String glossar();
+	
+}
