7841abe00b413f9709510942bc8a6839a29b29d3
Mihail Atanassov <atanassov.mihail@gmail.com>
2015-06-22 16:51:29 +0200

    Pre-Release adjustments

--- maalr.pg/src/main/webapp/assets/js/pledari.js
+++ maalr.pg/src/main/webapp/assets/js/pledari.js
@@ -1,10 +1,14 @@
 var retina = false;
 var locale;
+var u_name;
 
 $(document).ready(function() {
 	
 	/*Get the current locale value*/
 	locale = $('html').attr('lang');
+	
+	/*Get the user name*/
+	u_name = $('#u_name').text();
 	
 	checkMobile();
 	
@@ -73,9 +77,14 @@ $(document).ready(function() {
 //		$('#pt_brand_title').html('<a href="http://www.udg.ch/dicziunari/puter" target="_blank">PT</a>');
 //		$('#vl_brand_title').html('<a href="http://www.udg.ch/dicziunari/vallader" target="_blank">VL</a>');
 //		$('#sr_brand_title').html('<a href="http://www.vocabularisursilvan.ch" target="_blank">SR</a>');
+		
+		var short_u_name = u_name.replace('/[^\w+]/g');
+		short_u_name = short_u_name.substring(0, 2);
+		$('#u_name').text(short_u_name);
 	}
 
 	function stopMobile() {
+		// console.log("stopMobile");
 		
 		/*Reverse settings from startMobile function*/
 
@@ -104,6 +113,8 @@ $(document).ready(function() {
 //			$('#vl_brand_title').html('<a href="http://www.udg.ch/dicziunari/vallader" target="_blank">Vallader</a>');
 //			$('#sr_brand_title').html('<a href="http://www.vocabularisursilvan.ch" target="_blank">Sursilvan</a>');
 		}
+		
+		$('#u_name').text(u_name);
 	}
 
 	function initCustomElements() {
